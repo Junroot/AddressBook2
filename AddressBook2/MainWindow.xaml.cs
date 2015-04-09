6d4bcfd72448910c4adbaf4d1ffd544b964499df
ROOT <junroot0909@gmail.com>
2015-04-09 17:41:43 +0900

    04091741
    소트 구현함, ActBox 버튼 만듦

--- AddressBook2/MainWindow.xaml.cs
+++ AddressBook2/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,18 +27,41 @@ namespace AddressBook2
             AddressBookList.Items.Add(new User() { Name = "John Doe", Number = "01012345678" });
             AddressBookList.Items.Add(new User() { Name = "Jane Doe", Number = "01012345679" });
             AddressBookList.Items.Add(new User() { Name = "Sammy Doe", Number = "01012345680" });
+
+            AddressBookList.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Name",System.ComponentModel.ListSortDirection.Ascending));
+            AddressBookList.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Number", System.ComponentModel.ListSortDirection.Ascending));
         }
 
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult result;
+            result = MessageBox.Show("Do you want to close this window?", "", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
 
         private void AddressBookList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             User u = new User() { };
-            if (AddressBookList.SelectedItem != null)   u = (User)AddressBookList.SelectedItem;
-            NameBlock.Text = u.Name;
-            NumberBlock.Text = u.Number;
-            ListSelected.Visibility = Visibility.Visible;
-            Action.Visibility = Visibility.Collapsed;
-            AddBox.Visibility = Visibility.Collapsed;
+            if (AddressBookList.SelectedItem != null)
+            {
+                u = (User)AddressBookList.SelectedItem;
+                NameBlock.Text = u.Name;
+                NumberBlock.Text = u.Number;
+                ListSelected.Visibility = Visibility.Visible;
+                Action.Visibility = Visibility.Collapsed;
+                AddBox.Visibility = Visibility.Collapsed;
+                EditBox.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                ListSelected.Visibility = Visibility.Collapsed;
+                Action.Visibility = Visibility.Visible;
+                AddBox.Visibility = Visibility.Collapsed;
+                EditBox.Visibility = Visibility.Collapsed;
+            }
         }
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
@@ -47,12 +71,15 @@ namespace AddressBook2
                 AddBox.Visibility = Visibility.Visible;
                 Action.Visibility = Visibility.Collapsed;
                 ListSelected.Visibility = Visibility.Collapsed;
+                EditBox.Visibility = Visibility.Collapsed;
+
             }
             else if (AddBox.Visibility == Visibility.Visible)
             {
                 AddBox.Visibility = Visibility.Collapsed;
                 Action.Visibility = Visibility.Visible;
                 ListSelected.Visibility = Visibility.Collapsed;
+                EditBox.Visibility = Visibility.Collapsed;
                 NameBox.Text = String.Empty;
                 NumberBox.Text = String.Empty;
             }
@@ -60,6 +87,28 @@ namespace AddressBook2
 
         private void EditButton_Click(object sender, RoutedEventArgs e)
         {
+            if (EditBox.Visibility == Visibility.Collapsed)
+            {
+                if (AddressBookList.SelectedItem != null)
+                {
+                    User u = (User)AddressBookList.SelectedItem;
+                    AddBox.Visibility = Visibility.Collapsed;
+                    Action.Visibility = Visibility.Collapsed;
+                    ListSelected.Visibility = Visibility.Collapsed;
+                    EditBox.Visibility = Visibility.Visible;
+                    EditNameBox.Text = u.Name;
+                    EditNumberBox.Text = u.Number;
+                }
+
+            }
+            else if (EditBox.Visibility == Visibility.Visible)
+            {
+                AddBox.Visibility = Visibility.Collapsed;
+                Action.Visibility = Visibility.Visible;
+                ListSelected.Visibility = Visibility.Collapsed;
+                EditBox.Visibility = Visibility.Collapsed;
+
+            }
 
         }
 
@@ -80,15 +129,32 @@ namespace AddressBook2
         private void InsertButton_Click(object sender, RoutedEventArgs e)
         {
             int temp = AddressBookList.SelectedIndex;
-            AddressBookList.Items.Add(new User() { Name = NameBox.Text , Number = NumberBox.Text });
+            AddressBookList.Items.Add(new User() { Name = NameBox.Text, Number = NumberBox.Text });
             if (temp != -1)
             {
                 AddressBookList.SelectedItem = AddressBookList.Items[temp];
             }
             NameBox.Text = String.Empty;
             NumberBox.Text = String.Empty;
+            AddressBookList.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Name", System.ComponentModel.ListSortDirection.Ascending));
+            AddressBookList.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Number", System.ComponentModel.ListSortDirection.Ascending));
             AddBox.Visibility = Visibility.Collapsed;
+            ListSelected.Visibility = Visibility.Collapsed;
             Action.Visibility = Visibility.Visible;
+            EditBox.Visibility = Visibility.Collapsed;
+        }
+
+        private void ModifyButton_Click(object sender, RoutedEventArgs e)
+        {
+            ((User)AddressBookList.SelectedItem).Name = EditNameBox.Text;
+            ((User)AddressBookList.SelectedItem).Number = EditNumberBox.Text;
+            AddressBookList.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Name", System.ComponentModel.ListSortDirection.Ascending));
+            AddressBookList.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Number", System.ComponentModel.ListSortDirection.Ascending));
+            AddressBookList.Items.Refresh();
+            AddBox.Visibility = Visibility.Collapsed;
+            ListSelected.Visibility = Visibility.Collapsed;
+            Action.Visibility = Visibility.Visible;
+            EditBox.Visibility = Visibility.Collapsed;
         }
 
         private void ListViewItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -96,26 +162,32 @@ namespace AddressBook2
             var item = sender as ListViewItem;
             if (item != null && item.IsSelected)
             {
-                if (ListSelected.Visibility == Visibility.Collapsed )
+                if (ListSelected.Visibility == Visibility.Collapsed)
                 {
                     ListSelected.Visibility = Visibility.Visible;
                     Action.Visibility = Visibility.Collapsed;
                     AddBox.Visibility = Visibility.Collapsed;
+                    EditBox.Visibility = Visibility.Collapsed;
                 }
                 else if (ListSelected.Visibility == Visibility.Visible)
                 {
                     ListSelected.Visibility = Visibility.Collapsed;
                     Action.Visibility = Visibility.Visible;
                     AddBox.Visibility = Visibility.Collapsed;
+                    EditBox.Visibility = Visibility.Collapsed;
                 }
             }
         }
-    }
 
-    public class User
-    {
-        public string Name { get; set; }
+        void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        public class User
+        {
+            public string Name { get; set; }
 
-        public string Number { get; set; }
+            public string Number { get; set; }
+        }
     }
 }
